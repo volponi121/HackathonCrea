7ff5e7e89a7d84b2564e4c6ac98139288a169b3f
felipebassis <felipe.assis.it@gmail.com>
2019-08-17 22:15:27 -0300

    Corrigido o xibiu da coluna

--- hackaton-backend/src/main/java/br/com/hackathon/hackathon/controller/ArtController.java
+++ hackaton-backend/src/main/java/br/com/hackathon/hackathon/controller/ArtController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/art")
 public class ArtController {
--- hackaton-frontend/js/filtra.js
+++ hackaton-frontend/js/filtra.js
@@ -1,7 +1,7 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function() {
-    var dados = document.querySelectorAll(".nome");
+    var dados = document.querySelectorAll(".art");
 
     if (this.value.length > 0) {
         for (var i = 0; i < dados.length; i++) {
@@ -10,7 +10,7 @@ campoFiltro.addEventListener("input", function() {
             var nome = tdNome.textContent;
             var expressao = new RegExp(this.value, "i");
 
-            if (!expressao.test(nome)&&(!expressao.test())) {
+            if (!expressao.test(nome)) {
                 empresa.classList.add("invisivel");
             } else {
                 empresa.classList.remove("invisivel");
